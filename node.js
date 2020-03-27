09a3c08d997c75519655f97bc44ed617262da220
iskander1348 <58836824+iskander1348@users.noreply.github.com>
2020-03-27 17:19:07 +0700

    Add files via upload

---
+++ node.js
@@ -0,0 +1,68 @@
+/**
+ * Copyright 2016-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+var xhub = require('express-x-hub');
+
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'));
+
+app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
+app.use(bodyParser.json());
+
+var token = process.env.TOKEN || 'token';
+var received_updates = [];
+
+app.get('/', function(req, res) {
+  console.log(req);
+  
+  res.send('<pre>Ahoy!</br>' + JSON.stringify(received_updates, null, 2) + '</pre>');
+});
+
+app.get(['/facebook', '/instagram'], function(req, res) {
+  if (
+    req.query['hub.mode'] == 'subscribe' &&
+    req.query['hub.verify_token'] == token
+  ) {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+app.post('/facebook', function(req, res) {
+  console.log('Facebook request body:', req.body);
+
+  if (!req.isXHubValid()) {
+    console.log('Warning - request header X-Hub-Signature not present or invalid');
+    res.sendStatus(401);
+    return;
+  }
+
+  console.log('request header X-Hub-Signature validated');
+  // Process the Facebook updates here
+  received_updates.unshift(req.body);
+  call = 'https://app.leadconnect.cc/rest/v1/ext/add_call_api/?widget_key=b28e890c0ed858481130b297705dce6c&api_key=d56d22a8477faadd1ddbf7ce972a45619c4c5bc5&lc_number=+79835495859&country=RU'
+  xhr = new XMLHttpRequest();
+  xhr.open('get', call, true)
+  xhr.send()
+  
+  res.sendStatus(200);
+});
+
+app.post('/instagram', function(req, res) {
+  console.log('Instagram request body:');
+  console.log(req.body);
+  // Process the Instagram updates here
+  received_updates.unshift(req.body);
+  res.sendStatus(200);
+});
+
+app.listen();
