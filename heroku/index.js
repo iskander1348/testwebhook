1b13525b433c3b362394bb6344513be2c722d9de
iskander1348 <58836824+iskander1348@users.noreply.github.com>
2020-03-28 13:43:07 +0700

    Add files via upload

--- heroku/index.js
+++ heroku/index.js
@@ -25,28 +25,6 @@ var token = process.env.TOKEN || 'token';
 var received_updates = [];
 
 app.get('/', function(req, res) {
-  window.fbAsyncInit = function() {
-		FB.init({
-		  appId            : '2578575729127198,
-		  autoLogAppEvents : true,
-		  xfbml            : true,
-		  version          : 'v6.0'
-		});
-	  };
-	 
-  FB.login(function(response) {
-    if (response.authResponse) {
-     console.log('Welcome!  Fetching your information.... ');
-     FB.api('/me', function(response) {
-       console.log('Good to see you, ' + response.name + '.');
-     });
-    } else {
-     console.log('User cancelled login or did not fully authorize.');
-    }
-});
-
-  res.send('<script async defer src="https://connect.facebook.net/en_US/sdk.js"></script>')
-  res.send('<buton onclick="FB.login()">Login <>')
   console.log(req);
   res.send('<pre>Ahoy!</br>' + JSON.stringify(received_updates, null, 2) + '</pre>');
 });
